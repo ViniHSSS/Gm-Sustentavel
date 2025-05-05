9304dd3d670f8973e5f263961e1c9366f6bd4e83
ViniHSSS <viniciussilva1712v@gmail.com>
2025-05-05 00:36:28 +0000

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,8 @@
+function mostrarFormulario() {
+    const caixaInicial = document.getElementById('caixa-inicial');
+    const caixaFormulario = document.getElementById('caixa-formulario');
+  
+    caixaInicial.style.display = 'none';
+    caixaFormulario.style.display = 'block';
+  }
+  

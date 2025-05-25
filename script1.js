f0c33ed3913f190fb7f24a943f5f47b32dc7bbd9
ViniHSSS <viniciussilva1712v@gmail.com>
2025-05-25 17:22:40 +0000

    Delete script1.js

--- script1.js
+++
@@ -1,8 +0,0 @@
-function mostrarFormulario() {
-    const caixaInicial = document.getElementById('caixa-inicial');
-    const caixaFormulario = document.getElementById('caixa-formulario');
-  
-    caixaInicial.style.display = 'none';
-    caixaFormulario.style.display = 'block';
-  }
-  
